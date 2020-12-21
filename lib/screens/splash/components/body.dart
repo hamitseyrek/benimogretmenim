c5b55a0051c1f1fc6387bb9e14272cff7d73453c
hamitseyrek <hmtsyrk@gmail.com>
2020-12-21 15:46:16 +0300

    splash screen body title image edit style

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,3 +1,4 @@
+import 'package:benimogretmenim/ui/helper/constants.dart';
 import 'package:benimogretmenim/ui/helper/size_config.dart';
 import 'package:flutter/material.dart';
 
@@ -9,24 +10,39 @@ class Body extends StatefulWidget {
 class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Expanded(
-          flex: 3,
-          child: Column(
-            children: <Widget>[
-              Text(
-                "Benim Öğretmenim",
-                style: TextStyle(fontSize: getProportionateScreenWidth(36)),
-              )
-            ],
-          ),
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    "Benim Öğretmenim",
+                    style: TextStyle(
+                      fontSize: getProportionateScreenWidth(36),
+                      color: kPrimaryColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Bir sorun mu var. Çekinme sor bana!"),
+                  Image.asset(
+                    "assets/images/splash_1.png",
+                    height: getProportionateScreenHeight(265),
+                    width: getProportionateScreenWidth(235),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: SizedBox(),
+            ),
+          ],
         ),
-        Expanded(
-          flex: 2,
-          child: SizedBox(),
-        ),
-      ],
+      ),
     );
   }
 }
